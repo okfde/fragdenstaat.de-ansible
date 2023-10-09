db8380f22e96c5b015fc1fc1a0654045e01e6334
Denis <denis.witt@okfn.de>
2023-10-09 15:46:46 +0200

    Added pre-commit and fixed whitespace, eof, etc.

--- git_hooks/check_vault.sh
+++ git_hooks/check_vault.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FILES='secrets.yml|vpnclients.yml|*.vault|*.vault.yml'
+FILES='secrets\.yml|vpnclients\.yml|*\.vault|*\.vault.yml'
 
 for f in $(git diff --cached --name-only | grep -E ${FILES}); do
-    if [ ! $(head -n 1 ${f} | grep 'ANSIBLE_VAULT') ]; then
+    if [ ! $(git show :${f} | head -n 1 | grep 'ANSIBLE_VAULT') ]; then
 	echo "${f} is an unencrypted vault file, rejecting..."
 	echo "Please encrypt via 'ansible-vault' and try again."
 	exit 1
