e7cc27a594b3a62eb72ce2bda7a53f29ae3fbba0
Peymaneh <peymaneh@posteo.net>
2026-09-08 04:55:57 +0200

    Git hook: Fix grep regex and if conditional

--- git_hooks/check_vault.sh
+++ git_hooks/check_vault.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FILES='secrets\.yml|vpnclients\.yml|*\.vault|*\.vault.yml'
+FILES='secret(s?)\.y(a?)ml|vpnclients\.y(a?)ml|\.vault|\.vault.y(a?)ml'
 
 for f in $(git diff --cached --name-only | grep -E ${FILES}); do
-    if [ ! $(git show :${f} | head -n 1 | grep 'ANSIBLE_VAULT') ]; then
+    if ! git show :${f} | head -n 1 | grep 'ANSIBLE_VAULT'; then
 	echo "${f} is an unencrypted vault file, rejecting..."
 	echo "Please encrypt via 'ansible-vault' and try again."
 	exit 1
